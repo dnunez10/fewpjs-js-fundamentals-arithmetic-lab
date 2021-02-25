c614efa068d28a2d096524156b8f08c1dd771a47
dnunez10 <diana_nunez10@yahoo.com>
2021-02-24 16:15:54 -0800

    commit tree 126e369a6d3effbdcddba7f8cba718b862bfaa7d

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1 = 1;
+let num2 = 62;
+num1 * num2;
+
+let random = Math.round(Math.random() * 10);
+// Math.random() * 10;
+//     Number.isInteger();    
+
+let num3 = 88;
+let num4 = 12;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1, 2, 3, 20);
+
 
 // Write your code here
+
+
+
+
+let multiply = num1 * num2
